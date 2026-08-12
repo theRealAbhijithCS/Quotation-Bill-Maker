2aecb97cad480f3495a1d6c4d92e1a9c42785b05
Abhijith C S <136900298+abhijithcs200@users.noreply.github.com>
2026-08-12 11:33:13 +0530

    profile fix

--- m4_project/settings.py
+++ m4_project/settings.py
@@ -138,3 +138,9 @@ LOGIN_REDIRECT_URL = '/dashboard/'
 LOGOUT_REDIRECT_URL = '/login/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Data and File Upload Settings (Prevents RequestDataTooBig for large forms/uploads)
+DATA_UPLOAD_MAX_MEMORY_SIZE = int(os.environ.get('DATA_UPLOAD_MAX_MEMORY_SIZE', 52428800))  # 50 MB
+FILE_UPLOAD_MAX_MEMORY_SIZE = int(os.environ.get('FILE_UPLOAD_MAX_MEMORY_SIZE', 52428800))  # 50 MB
+DATA_UPLOAD_MAX_NUMBER_FIELDS = int(os.environ.get('DATA_UPLOAD_MAX_NUMBER_FIELDS', 10000))
+
